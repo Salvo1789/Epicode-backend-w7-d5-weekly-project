48d2b37261d8c9d7948c5611573657ed184354b6
“Salvo1789” <salvo.mercurio89@gmail.com>
2023-06-16 17:02:47 +0200

    Fixed exception catch

--- GestioneIncendi/src/main/java/salvomercurio/GestioneIncendi/GestioneIncendiApplication.java
+++ GestioneIncendi/src/main/java/salvomercurio/GestioneIncendi/GestioneIncendiApplication.java
@@ -22,34 +22,33 @@ public class GestioneIncendiApplication {
 		try {
 			Sonda sonda1 = new Sonda(smokeDetector1, 100341, 32.1865, 17.9963);
 			Sonda sonda2 = new Sonda(smokeDetector2, 100342, 15.0497, 86.0432);
-			Sonda sonda3 = new Sonda(smokeDetector3, 100343, 91.0432, 73.6753);
+
+			System.out.println("Sonda 1");
+			System.out.println("Livello del fumo: 1");
+			smokeDetector1.setSmokeLevel(sonda1, 1);
+			System.out.println("Livello del fumo: 3");
+			smokeDetector1.setSmokeLevel(sonda1, 3);
+			System.out.println("Livello del fumo: 5");
+			smokeDetector1.setSmokeLevel(sonda1, 5);
+			System.out.println("Livello del fumo: 7");
+			smokeDetector1.setSmokeLevel(sonda1, 7);
+			System.out.println("Livello del fumo: 9");
+			smokeDetector1.setSmokeLevel(sonda1, 9);
+
+			System.out.println("Sonda 2");
+			System.out.println("Livello del fumo: 1");
+			smokeDetector2.setSmokeLevel(sonda2, 1);
+			System.out.println("Livello del fumo: 3");
+			smokeDetector2.setSmokeLevel(sonda2, 3);
+			System.out.println("Livello del fumo: 5");
+			smokeDetector2.setSmokeLevel(sonda2, 5);
+			System.out.println("Livello del fumo: 7");
+			smokeDetector2.setSmokeLevel(sonda2, 7);
+			System.out.println("Livello del fumo: 9");
+			smokeDetector2.setSmokeLevel(sonda2, 9);
 		} catch (InvalidCoordinatesException e) {
 			e.printStackTrace();
 		}
-
-		System.out.println("Sonda 1");
-		System.out.println("Livello del fumo: 1");
-		smokeDetector1.setSmokeLevel(1);
-		System.out.println("Livello del fumo: 3");
-		smokeDetector1.setSmokeLevel(3);
-		System.out.println("Livello del fumo: 5");
-		smokeDetector1.setSmokeLevel(5);
-		System.out.println("Livello del fumo: 7");
-		smokeDetector1.setSmokeLevel(7);
-		System.out.println("Livello del fumo: 9");
-		smokeDetector1.setSmokeLevel(9);
-
-		System.out.println("Sonda 2");
-		System.out.println("Livello del fumo: 1");
-		smokeDetector2.setSmokeLevel(1);
-		System.out.println("Livello del fumo: 3");
-		smokeDetector2.setSmokeLevel(3);
-		System.out.println("Livello del fumo: 5");
-		smokeDetector2.setSmokeLevel(5);
-		System.out.println("Livello del fumo: 7");
-		smokeDetector2.setSmokeLevel(7);
-		System.out.println("Livello del fumo: 9");
-		smokeDetector2.setSmokeLevel(9);
 
 	}
 }
